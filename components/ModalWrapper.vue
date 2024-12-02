c3ffabde8012730c8b37df1a914d1514726f31d0
Johannes Abendroth <johannes.abendroth@gmail.com>
2024-12-02 14:10:24 +0100

    add keyup close to modal

--- components/ModalWrapper.vue
+++ components/ModalWrapper.vue
@@ -3,12 +3,31 @@
     <div class="bg-white border-5 border-green p-8">
       <slot></slot>
     </div>
-    <button @click="setCurrentModal(null)" class="absolute top-8 right-8 text-white">X</button>
+    <button @click="onClickClose" class="absolute top-8 right-8 text-white">X</button>
   </div>
 </template>
 
 <script setup>
-import { inject } from 'vue';
+import { inject, onMounted, onBeforeUnmount } from 'vue';
 
 const { setCurrentModal } = inject('modal');
+
+onMounted(() => {
+  document.addEventListener('keyup', onKeyUpClose);
+});
+
+onBeforeUnmount(() => {
+  document.removeEventListener('keyup', onKeyUpClose);
+});
+
+function onKeyUpClose(event) {
+  if (event.code === 'Escape') {
+    setCurrentModal(null);
+  }
+}
+
+function onClickClose() {
+  setCurrentModal(null);
+}
+
 </script>
