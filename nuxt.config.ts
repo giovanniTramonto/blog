f1d8f2405508b2aea4be7f5a50d16a3f92d71247
Johannes Abendroth <johannes.abendroth@gmail.com>
2024-12-05 22:28:36 +0100

    improve head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Posts',
+      htmlAttrs: {
+        lang: 'en'
+      }
+    }
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   modules: [
